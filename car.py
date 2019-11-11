486793076dc39fe3e9f076a8d8ce7f019e6ab3bb
fullere <fullere@hartwick.edu>
2019-11-11 16:00:27 -0500

    Created class Car

--- car.py
+++ car.py
@@ -1,3 +1,31 @@
 # Elizabeth Fuller
 # 11/11/19
 # Car Class
+
+
+class Car:
+    def __init__(self, make, model, color, year, mileage, price):
+        self.make = make
+        self.model = model
+        self.color = color
+        self.year = year
+        self.mileage = mileage
+        self.price = price
+
+    def description(self):
+        print(f"""The car's attributes:
+Make: {self.make}
+Model: {self.model}
+Color: {self.color}
+Year: {self.year}
+Mileage: {self.mileage}
+Price: {self.price}""")
+
+    def change_color(self):
+        new_color = input("Change car color to: ")
+        self.color = new_color
+
+
+    def update_odometer(self):
+        new_reading = int(input("Enter new odometer reading: "))
+        self.mileage = new_reading
---
+++ test_class.py
@@ -0,0 +1,12 @@
+# elizabeth Fuller
+# 11/11/19
+# code to test out the car class
+from car import Car
+
+
+my_car = Car("Ford", "Mustang", "red", 2016, 1900, 250000)
+
+Car.description(my_car)
+Car.change_color(my_car)
+Car.update_odometer(my_car)
+Car.description(my_car)
